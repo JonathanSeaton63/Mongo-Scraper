9fc93af04880d4b2062a2587daf9cbf6f6bbc7c4
Jon Seaton <jonathanseaton29@gmail.com>
2019-03-23 13:55:13 -0600

    updating files

---
+++ scripts/scrape.js
@@ -0,0 +1,28 @@
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+const scrape = function(cb) {
+  axios.get("https://www.deseretnews.com/", function(err, res, body){
+    const $ = cheerio.load(body);
+    const articles = [];
+
+    $(".main-column").each(function(i, element){
+      const head = $(this).children(".title").text().trim();
+      const sum = $(this).children(".summary").text().trim();
+
+      if(head && sum){
+        var headNeat = head.replace(/(\r\n|\n|\r|\t|\s+)/gm," ").trim();
+        var sumNeat = sum.replace(/(\r\n|\n|\r|\t|\s+)/gm," ").trim();
+
+        const dateToAdd = {
+          headline: headNeat,
+          summary: sumNeat
+        };
+        articles.push(dateToAdd);
+      }
+    });
+    cb(articles);
+  });
+};
+
+module.exports = scrape;
